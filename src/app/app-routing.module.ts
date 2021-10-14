e8e9bc20175432fe72ba0944fcce8bc4315d0560
Hamit Enes Gümüş <hamitenes@hotmail.com>
2021-10-14 10:23:54 +0300

    Karşılama ekranı eklendi.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   {
     path: 'user',
     loadChildren: () => import('./user/user.module').then((m) => m.UserModule),
+  },
+  {
+    path: '',
+    component: HomeComponent,
   },
 ];
 
